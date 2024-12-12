301c1c4f3d3efd74c8240bd7445d6a2906db8ad6
parkdaihee <park827401@naver.com>
2024-12-12 15:35:28 +0900

    adsdasf

--- db/seed.ts
+++ db/seed.ts
@@ -23,7 +23,18 @@ const main = async () => {
       .insert(schema.products)
       .values(sampleData.products)
       .returning()
+
     console.log({ resProducts })
+    await db.delete(schema.accounts)
+    await db.delete(schema.users)
+
+    const resUsers = await db
+      .insert(schema.users)
+      .values(sampleData.users)
+      .returning()
+
+    console.log({ resUsers, resProducts })
+
     await client.end()
   } catch (error) {
     console.error(error)
